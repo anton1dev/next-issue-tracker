3ab936797c81640a36b7a74b75614c21e7d8219c
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-08-21 09:15:17 +0300

    added loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
 import { Card, Flex, Heading } from '@radix-ui/themes'
+import delay from 'delay'
 import { notFound } from 'next/navigation'
 import ReactMarkdown from 'react-markdown'
 
