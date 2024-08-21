8daa991c16c5bf9384029b1f904f47b9de83678f
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-08-21 12:46:08 +0300

    show a confirm dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,17 +1,34 @@
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+'use client'
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
 
 interface Props {
     id: number
 }
 
-const DeleteIssueButton = ({id}: Props) => {
+const DeleteIssueButton = ({ id }: Props) => {
     return (
-        <Button color='red'>
-            <Pencil2Icon  />
-            <Link href={`/issues/${id}/delete`}>Delete Issue</Link>
-        </Button>
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <Button color='red'>
+                    Delete Issue
+                </Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Confirm Deletion?</AlertDialog.Title>
+                <AlertDialog.Description>You're deleting this issue, is it okay?</AlertDialog.Description>
+                <Flex direction='row' gap='2' mt='2'>
+                    <AlertDialog.Action>
+                        <Button color='lime'>OK</Button>
+                    </AlertDialog.Action>
+                    <AlertDialog.Cancel>
+                        <Button color='cyan'>Cancel</Button>
+                    </AlertDialog.Cancel>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
+
     )
 }
 
