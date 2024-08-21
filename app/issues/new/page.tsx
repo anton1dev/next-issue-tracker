e9705d4cee3bdf133b8ab9e58174e5507fd59e7c
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-08-21 09:18:53 +0300

    disable SSR for loading markdown

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,19 @@
 'use client';
 
-import { Button, Callout, TextField, Text } from '@radix-ui/themes'
-import SimpleMDE from 'react-simplemde-editor';
-import { useForm, Controller } from 'react-hook-form';
-import 'easymde/dist/easymde.min.css';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import 'easymde/dist/easymde.min.css';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -50,10 +52,10 @@ const NewIssuePage = () => {
                     render={({ field }) => <SimpleMDE placeholder='Description' {...field} />
                     }
                 />
-                    <ErrorMessage>{errors.description?.message}
-                    </ErrorMessage>
+                <ErrorMessage>{errors.description?.message}
+                </ErrorMessage>
                 <Button disabled={submitting}>
-                    Submit New Issue{submitting && <Spinner/>}
+                    Submit New Issue{submitting && <Spinner />}
                 </Button>
             </form>
         </div>
