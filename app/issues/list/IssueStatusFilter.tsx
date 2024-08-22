126f6ee70bceec2bf8388c6150c120bfa4526e45
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-08-22 14:36:33 +0300

    made columns sortable

---
+++ app/issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Status } from '@prisma/client'
+import { Select } from '@radix-ui/themes'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const IssueStatusFilter = () => {
+    const router = useRouter();
+
+    const statuses: {label: string, value?: Status}[] = [
+        {label: 'All'},
+        {label: 'Open', value: 'OPEN'},
+        {label: 'Closed', value: 'CLOSED'},
+        {label: 'In Progress', value:'IN_PROGRESS'}
+    ]
+  return (
+    <Select.Root onValueChange={(status) => {
+        const query = status ? `?status=${status}` : '';
+        router.push(query);
+    } }>
+        <Select.Trigger placeholder="Filter by status..."/>
+        <Select.Content>
+            {statuses.map(status => 
+                <Select.Item key={status.value} value={status.value || ''}>
+                    {status.label}
+                </Select.Item>)}
+        </Select.Content>
+    </Select.Root>
+  )
+}
+
+export default IssueStatusFilter
