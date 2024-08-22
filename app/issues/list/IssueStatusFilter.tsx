5732d8c5cde0ae116050df2bd678ea4b046b30f1
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-08-22 15:14:15 +0300

    Added filtration based on searchParams

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,32 +2,37 @@
 
 import { Status } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const IssueStatusFilter = () => {
     const router = useRouter();
+    const searchParams = useSearchParams();
 
-    const statuses: {label: string, value?: Status}[] = [
-        {label: 'All'},
-        {label: 'Open', value: 'OPEN'},
-        {label: 'Closed', value: 'CLOSED'},
-        {label: 'In Progress', value:'IN_PROGRESS'}
+    const statuses: { label: string, value?: Status }[] = [
+        { label: 'All' },
+        { label: 'Open', value: 'OPEN' },
+        { label: 'Closed', value: 'CLOSED' },
+        { label: 'In Progress', value: 'IN_PROGRESS' }
     ]
-  return (
-    <Select.Root onValueChange={(status) => {
-        const query = status ? `?status=${status}` : '';
-        router.push(query);
-    } }>
-        <Select.Trigger placeholder="Filter by status..."/>
-        <Select.Content>
-            {statuses.map(status => 
-                <Select.Item key={status.value} value={status.value || ''}>
-                    {status.label}
-                </Select.Item>)}
-        </Select.Content>
-    </Select.Root>
-  )
+    return (
+        <Select.Root defaultValue={searchParams.get('status') || ''} onValueChange={(status) => {
+            const params = new URLSearchParams();
+            if (status) params.append('status', status);
+            if (searchParams.get('orderBy')) params.append('orderBy', searchParams.get('orderBy')!)
+            
+            const query = params.size ? '?' + params.toString() : '';
+            router.push('/issues/list' + query);
+        }}>
+            <Select.Trigger placeholder="Filter by status..." />
+            <Select.Content>
+                {statuses.map(status =>
+                    <Select.Item key={status.value} value={status.value || ''}>
+                        {status.label}
+                    </Select.Item>)}
+            </Select.Content>
+        </Select.Root >
+    )
 }
 
 export default IssueStatusFilter
